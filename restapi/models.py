298b35c8e835643c661f3e9738944c28a52a6469
abhishek-dhanraj-TI <abhishek.dhanraj@trilogy.com>
2022-05-21 16:04:02 +0000

    Added str() for models

--- restapi/models.py
+++ restapi/models.py
@@ -9,11 +9,16 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=200, null=False)
+    def __str__(self) -> str:
+        return f"name: {self.name}"
 
 
 class Groups(models.Model):
     name = models.CharField(max_length=100, null=False)
     members = models.ManyToManyField(User, related_name='members', blank=True)
+    
+    def __str__(self) -> str:
+        return f"name: {self.name}, members: {self.members}"
 
 
 class Expenses(models.Model):
@@ -22,6 +27,8 @@ class Expenses(models.Model):
     group = models.ForeignKey(Groups, null=True, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return f"description: {self.description}, total_amount: {self.total_amount}, group: {self.group}, category: {self.category}"
 
 class UserExpense(models.Model):
     expense = models.ForeignKey(Expenses, default=1, on_delete=models.CASCADE, related_name="users")
@@ -30,4 +37,4 @@ class UserExpense(models.Model):
     amount_lent = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self) -> str:
-        return f"user: {self.user}, amount_owed: {self.amount_owed} amount_lent: {self.amount_lent}"
+        return f"user: {self.user}, amount_owed: {self.amount_owed}, amount_lent: {self.amount_lent}"
